fc7b75d108c19a116ef8f81f3e1a5fefa1fad866
hibana2077 <hibana2077@gmail.com>
2024-11-02 14:39:07 +0800

    更新 COCO 2017 數據集下載腳本，新增資料夾結構以整理下載的檔案

--- OmniPose/data/coco/download_dataset.sh
+++ OmniPose/data/coco/download_dataset.sh
@@ -1,15 +1,16 @@
+sudo apt update || apt update &&
 sudo apt install wget curl unzip -y || apt install wget curl unzip -y &&
 
 curl -O -q http://images.cocodataset.org/zips/train2017.zip &&
-unzip -d train2017 train2017.zip &&
-rm train2017.zip &&
 
 curl -O -q http://images.cocodataset.org/zips/val2017.zip &&
-unzip -d val2017 val2017.zip &&
-rm val2017.zip &&
 
 curl -O -q http://images.cocodataset.org/annotations/annotations_trainval2017.zip &&
-unzip -d annotations annotations_trainval2017.zip &&
-rm annotations_trainval2017.zip &&
+
+mkdir -p coco &&
+mkdir -p coco/annotations &&
+mkdir -p coco/images &&
+
+unzip -q annotations_trainval2017.zip -d coco/annotations &&
 
 echo "Downloaded COCO 2017 dataset"
