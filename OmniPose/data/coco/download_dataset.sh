5cc09a4c3f5dee7ec3d5f9337ed47e9e4d7ee50b
hibana2077 <hibana2077@gmail.com>
2024-11-02 14:44:26 +0800

    更新 COCO 2017 數據集下載腳本，調整資料夾結構以簡化檔案管理

--- OmniPose/data/coco/download_dataset.sh
+++ OmniPose/data/coco/download_dataset.sh
@@ -7,10 +7,9 @@ curl -O -q http://images.cocodataset.org/zips/val2017.zip &&
 
 curl -O -q http://images.cocodataset.org/annotations/annotations_trainval2017.zip &&
 
-mkdir -p coco &&
-mkdir -p coco/annotations &&
-mkdir -p coco/images &&
+mkdir -p annotations &&
+mkdir -p images &&
 
-unzip -q annotations_trainval2017.zip -d coco/annotations &&
+unzip -q annotations_trainval2017.zip -d annotations &&
 
 echo "Downloaded COCO 2017 dataset"
